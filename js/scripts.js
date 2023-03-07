1bed4f26b8b91b8e06e772c3b3421f6b80f82c33
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-03-07 10:50:26 +0100

    Create list-variable and add 3 sample objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-let favouriteFood = "Pizza";
-alert('Hello World!');
-document.write('My favourite food is ', favouriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass','poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water']
+    }
+];
