4f41576c933582d73e23f6da9acaebc45002180d
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-03-11 14:07:51 +0100

    Create list of pokemons and loop to display on index page

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,56 @@ let pokemonList = [
         types: ['grass','poison']
     },
     {
+        name: 'Ivysaur',
+        height: 1.0,
+        types: ['grass','poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2.0,
+        types: ['grass','poison']
+    },
+    {
         name: 'Charmander',
         height: 0.6,
         types: ['fire']
     },
     {
+        name: 'Charmeleon',
+        height: 1.1,
+        types: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire','flying']
+    },
+    {
         name: 'Squirtle',
         height: 0.5,
         types: ['water']
+    },
+    {
+        name: 'Wartortle',
+        height: 1.0,
+        types: ['water']
+    },
+    {
+        name: 'Blastoise',
+        height: 1.6,
+        types: ['water']
     }
 ];
+
+let big = 1.5
+
+
+for (let i=0; i<pokemonList.length; i++){
+    if (pokemonList[i].height >= big){
+        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") -> <b>Wow, that's big!</b></p>");
+    }
+    else {
+        document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>");
+    } 
+}
+
