a4ec9182cc56614d8ab931fd9b05c6a4c19628cb
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-03-16 12:09:57 +0100

    Wrap pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,70 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 1.0,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2.0,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        name: 'Charmeleon',
-        height: 1.1,
-        types: ['fire']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire','flying']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Wartortle',
-        height: 1.0,
-        types: ['water']
-    },
-    {
-        name: 'Blastoise',
-        height: 1.6,
-        types: ['water']
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Ivysaur',
+            height: 1.0,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Venusaur',
+            height: 2.0,
+            types: ['grass','poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            name: 'Charmeleon',
+            height: 1.1,
+            types: ['fire']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire','flying']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        },
+        {
+            name: 'Wartortle',
+            height: 1.0,
+            types: ['water']
+        },
+        {
+            name: 'Blastoise',
+            height: 1.6,
+            types: ['water']
+        }
+     ]                     
+   
+    function getAll () {
+      return pokemonList;
     }
-];
+  
+    function add () {
+      pokemonList.push(pokemon);
+    }
+  
+    return {
+      getAll,
+      add
+    }
+                      
+  })();
+
 
 let big = 1.5
-
 
 function printAllPokemons (pokemon){
     if (pokemon.height >= big){
@@ -55,6 +72,7 @@ function printAllPokemons (pokemon){
     }
     else {
         document.write("<p>" + pokemon.name + " (height: " + pokemon.height + " m)</p>");
-    } }
+    } 
+}
 
- pokemonList.forEach(printAllPokemons);
+ pokemonRepository.getAll().forEach(printAllPokemons);
